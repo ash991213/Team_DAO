f0554bbae0f3c855a547742089966f4acb213d4e
안수환 <ansuhwan@ansuhwan-ui-MacBookAir.local>
2022-09-26 10:21:41 +0900

    contract 재배포

--- oAuth_Server/back/routers/user/utils.ts
+++ oAuth_Server/back/routers/user/utils.ts
@@ -1,2 +1,2 @@
-// export const frontend = 'http://teamdid.shop';
-export const frontend = 'http://localhost:8080';
+export const frontend = 'http://teamdid.shop';
+// export const frontend = 'http://localhost:8080';
